0320a2c548dc5efbc8c6bb30f5e99e6ae5085e4f
MirzaAyon <ayonmirza98@gmail.com>
2022-08-08 12:57:39 +0600

    every products are showing with price,seller and ratings

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -9,7 +9,15 @@ const Product = (props) => {
         <div className='product'>
             {/* <h2>This is product</h2> */}
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>price: ${price}</p>
+                <p> <small>Seller: {seller}</small> </p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
