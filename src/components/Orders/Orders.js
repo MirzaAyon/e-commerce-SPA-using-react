37109837fd2e4888d5c99c4c1cb713ec55a5ccf6
MirzaAyon <ayonmirza98@gmail.com>
2022-08-20 14:39:44 +0600

    review items and cart are showing in orders section

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useproducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
@@ -8,11 +10,26 @@ const Orders = () => {
     const [cart, setCart] = useCart(products)
     return (
         <div>
-            <h2>This is orders : {products.length}
+            {/* <h2>This is orders : {products.length}
             </h2>
-            <p>Cart has : {cart.length}</p>
+            <p>Cart has : {cart.length}</p> */}
             {/* kintu orders e mot koyta eta na dekhay item koyta sheta dekhachee */}
             {/* hobar kotha 5 dekhae 3  */}
+
+            <div className='shop-container'>
+                <div className='products-container'>
+                    {
+                        cart.map(product => <ReviewItem key={product.id} product={product} >
+
+                        </ReviewItem>)
+                    }
+                </div>
+                <div className='cart-container'>
+                    <Cart cart={cart}></Cart>
+                </div>
+
+            </div>
+
         </div>
     );
 };
