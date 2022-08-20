fa0cd896acb76827070a2273216046f36f6cfb3b
MirzaAyon <ayonmirza98@gmail.com>
2022-08-20 09:20:11 +0600

    our all the three routes are working

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is about</h2>
+            <p>we are secret but you can't</p>
+        </div>
+    );
+};
+
+export default About;
